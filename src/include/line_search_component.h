d656acd7c4a096329e24f29125057f20ef3be01f
Shintaro Kaneko <kaneshin0120@gmail.com>
2012-06-21 04:24:19 +0900

    Create a component of line search to be easily.

--- src/include/line_search_component.h
+++ src/include/line_search_component.h
@@ -1,8 +1,6 @@
 #ifndef OPTIMIZATION_LINE_SEARCH_H
 #define OPTIMIZATION_LINE_SEARCH_H
 
-#include "non_linear_component.h"
-
 enum LineSearchStatus {
     LINE_SEARCH_FUNCTION_NAN = -1,
     LINE_SEARCH_SATISFIED = 0,
@@ -10,77 +8,16 @@ enum LineSearchStatus {
     LINE_SEARCH_STEP_WIDTH_FAILED,
 };
 
-int line_search_armijo
-(
-    const double *x,
-    const double *g,
-    const double *d,
-    int n,
-    double step_width,
-    double tau,
-    double xi,
-    NonLinearComponent *component
-);
-
-int
-line_search_wolfe
-(
-    const double *x,
-    const double *g,
-    const double *d,
-    int n,
-    double step_width,
-    double tau,
-    double xi,
-    double sigma,
-    NonLinearComponent *component
-);
-
-int
-line_search_strong_wolfe
-(
-    const double *x,
-    const double *g,
-    const double *d,
-    int n,
-    double step_width,
-    double tau,
-    double xi,
-    double sigma,
-    NonLinearComponent *component
-);
-
-int
-line_search_backtracking_wolfe
-(
-    const double *x,
-    const double *g,
-    const double *d,
-    int n,
-    double step_width,
-    double tau,
-    double xi,
-    double sigma,
-    double decreasing,
-    double increasing,
-    NonLinearComponent *component
-);
-
-int
-line_search_backtracking_strong_wolfe
-(
-    const double *x,
-    const double *g,
-    const double *d,
-    int n,
-    double step_width,
-    double tau,
-    double xi,
-    double sigma,
-    double decreasing,
-    double increasing,
-    NonLinearComponent *component
-);
+typedef struct _LineSearchParameter {
+    double tolerance;
+    int max_iteration;
+    double step_width;
+    double xi;
+    double tau;
+    double sigma;
+    double decreasing;
+    double increasing;
+} LineSearchParameter;
 
 #endif // OPTIMIZATION_LINE_SEARCH_H
 
