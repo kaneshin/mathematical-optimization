b56fe15af2af3db9f4716c0c967b6d5c1711861f
Shintaro Kaneko <kaneshin0120@gmail.com>
2012-07-02 19:33:06 +0900

    Merge methematical libraries into mymath.c.

--- src/include/mymath.h
+++ src/include/mymath.h
@@ -1,3 +1,12 @@
+/*
+ * vim:set ts=8 sts=4 sw=4 tw=0:
+ *
+ * File:        mymath.h
+ * Version:     0.1.0
+ * Maintainer:  Shintaro Kaneko <kaneshin0120@gmail.com>
+ * Last Change: 02-Jul-2012.
+ */
+
 #ifndef OPTIMIZATION_MYMATH_H
 #define OPTIMIZATION_MYMATH_H
 
@@ -10,21 +19,53 @@ enum MyMathStatus {
     MY_MATH_NOT_UPDATE,
 };
 
-int gauss_seidel
-(
+double
+dot_product(
+    const double *x,
+    const double *y,
+    unsigned int n
+);
+
+void
+update_step_vector(
+    double *x_temp,
+    const double *x,
+    double alpha,
+    const double *y,
+    unsigned int n
+);
+
+double
+manhattan_norm(
+    const double *x,
+    unsigned int n
+);
+
+double
+euclidean_norm(
+    const double *x,
+    unsigned int n
+);
+
+double
+infinity_norm(
+    const double *x,
+    unsigned int n
+);
+
+int gauss_seidel(
     double **a,
     double *x,
     const double *b,
-    int n,
+    unsigned int n,
     double epsilon
 );
 
-int successive_over_relaxation
-(
+int successive_over_relaxation(
     double **a,
     double *x,
     const double *b,
-    int n,
+    unsigned int n,
     double epsilon,
     double omega
 );
--- src/mymath.c
+++ src/mymath.c
@@ -1,79 +1,149 @@
-// vim:set ts=8 sts=4 sw=4 tw=0:
-// vim:set foldmethod=marker foldmarker={{{,}}}:
-/* ===========================================================================
- *  File: mymath.c
- *  Version: 0.9.0
- *  Last Change: 18-May-2012.
- *  Maintainer: Shintaro Kaneko <kaneshin0120@gmail.com>
- *  Description:
-=========================================================================== */
+/*
+ * vim:set ts=8 sts=4 sw=4 tw=0:
+ *
+ * File:        mymath.c
+ * Version:     0.1.0
+ * Maintainer:  Shintaro Kaneko <kaneshin0120@gmail.com>
+ * Last Change: 02-Jul-2012.
+ */
 
 #include "include/mymath.h"
 
 #include <math.h>
 
+/*
+ * Libraries of Vector
+ *  - dot_product
+ *  - update_step_vector
+ *  - manhattan_norm
+ *  - euclidean_norm
+ *  - infinity_norm
+ */
+double
+dot_product(
+    const double *x,
+    const double *y,
+    unsigned int n
+) {
+    unsigned int i;
+    double dot;
+    for (i = 1, dot = x[0] * y[0]; i < n; ++i)
+        dot += x[i] * y[i];
+    return dot;
+}
+
+void
+update_step_vector(
+    double *x_temp,
+    const double *x,
+    double alpha,
+    const double *y,
+    unsigned int n
+) {
+    unsigned int i;
+    for (i = 0; i < n; ++i)
+        x_temp[i] = x[i] + alpha * y[i];
+}
+
+double
+manhattan_norm(
+    const double *x,
+    unsigned int n
+) {
+    /*
+     * norm = |x_0| + |x_1| + ... + |x_n|
+     */
+    unsigned int i;
+    double norm, fabs_x;
+    for (i = 1, norm = fabs(x[0]); i < n; ++i)
+        norm += fabs(x[i]);
+    return norm;
+}
+
+double
+euclidean_norm(
+    const double *x,
+    unsigned int n
+) {
+    /*
+     * norm = sqrt(x_0 * x_0 + x_1 * x_1 + ... + x_n * x_n)
+     */
+    unsigned int i;
+    double norm;
+    for (i = 1, norm = x[0] * x[0]; i < n; ++i)
+        norm += x[i] * x[i];
+    return sqrt(norm);
+}
+
+double
+infinity_norm(
+    const double *x,
+    unsigned int n
+) {
+    /*
+     * norm = max(|x_0|, |x_1|, ..., |x_n|)
+     */
+    unsigned int i;
+    double norm, fabs_x;
+    for (i = 1, norm = fabs(x[0]); i < n; ++i) {
+        fabs_x = fabs(x[i]);
+        if (fabs_x > norm)
+            norm = fabs_x;
+    }
+    return norm;
+}
+
+/*
+ * Libraries of Methematical Analysis
+ *  - gauss_seidel
+ *  - successive_over_relaxation
+ */
 int
-gauss_seidel
-(
+gauss_seidel(
     double **a,
     double *x,
     const double *b,
-    int n,
+    unsigned int n,
     double epsilon
-)
-{
-    int i, j;
-    double norm, temp, x_old;
-    do {
-        norm = 0.;
-        for (i = 0; i < n; i++) {
-            x_old = x[i];
-            temp = b[i];
-            for (j = 0; j < i; j++) {
-                temp -= a[i][j] * x[j];
-            }
-            for (j = i + 1; j < n; j++) {
-                temp -= a[i][j] * x[j];
-            }
-            if (0 != a[i][i]) {
-                x[i] = temp / a[i][i];
-            }
-            temp = fabs(x[i] - x_old);
-            if (norm < temp) norm = temp;
-        }
-    } while(norm > epsilon);
-    return MY_MATH_SATISFIED;
+) {
+    /*
+     * NOTE:
+     *  If you choose the value of Omega as Zero with SOR method, SOR method
+     *  become Gause-Seidel method.
+     */
+    return successive_over_relaxation(a, x, b, n, epsilon, 1.);
 }
 
 int
-successive_over_relaxation
-(
+successive_over_relaxation(
     double **a,
     double *x,
     const double *b,
-    int n,
+    unsigned int n,
     double epsilon,
     double omega
-)
-{
-    int i, j;
+) {
+    /*
+     * TODO:
+     *  Error Handling if you get a value of singular as Zero.
+     */
+    unsigned int i, j;
     double norm, temp, x_old;
     do {
-        norm = 0.;
-        for (i = 0; i < n; i++) {
+        for (i = 0, norm = 0.; i < n; ++i) {
             x_old = x[i];
             temp = b[i];
-            for (j = 0; j < i; j++) {
+            for (j = 0; j < i; ++j)
                 temp -= a[i][j] * x[j];
-            }
-            for (j = i + 1; j < n; j++) {
+            for (j = i + 1; j < n; ++j)
                 temp -= a[i][j] * x[j];
-            }
-            if (0 != a[i][i]) {
+            if (0. != a[i][i])
                 x[i] = x_old + omega * (temp / a[i][i] - x_old);
-            }
+            else
+                return MY_MATH_FAILED;
             temp = fabs(x[i] - x_old);
-            if(norm < temp) norm = temp;
+            if(norm < temp)
+                norm = temp;
         }
     } while(norm > epsilon);
     return MY_MATH_SATISFIED;
