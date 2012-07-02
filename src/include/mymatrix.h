a7dcecbd28e367436a04660b0e385dae1d2d04e9
Shintaro Kaneko <kaneshin0120@gmail.com>
2012-07-02 19:38:05 +0900

    Delete mymatrix.

--- src/include/mymatrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPTIMIZATION_MYMATRIX_H
-#define OPTIMIZATION_MYMATRIX_H
-
-void matrix_delete(double **matrix);
-
-void identity_matrix(double **a, int n);
-
-#endif // OPTIMIZATION_MYMATRIX_H
-
